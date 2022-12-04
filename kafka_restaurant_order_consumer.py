fa06c61329db31c1f523ae73492d96fae599a27f
renuka1221 <113676362+renuka1221@users.noreply.github.com>
2022-12-04 23:27:28 +0530

    Create kafka_restaurant_order_consumer.py

---
+++ kafka_restaurant_order_consumer.py
@@ -0,0 +1,95 @@
+import argparse
+import pandas as pd
+from confluent_kafka import Consumer
+from confluent_kafka.serialization import SerializationContext, MessageField
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.json_schema import JSONDeserializer
+import os
+
+API_KEY = 'UNPVDP5DOZCH6G73'
+ENDPOINT_SCHEMA_URL = 'https://psrc-qjmzd.us-east-2.aws.confluent.cloud'
+API_SECRET_KEY = '7Ly3csDUJRDTArGeOgerlkge0Lbeabguh8glIIRZF1GR5dsNYjLzSii/O1Q1+XRx'
+BOOTSTRAP_SERVER = 'pkc-ymrq7.us-east-2.aws.confluent.cloud:9092'
+SECURITY_PROTOCOL = 'SASL_SSL'
+SSL_MACHENISM = 'PLAIN'
+SCHEMA_REGISTRY_API_KEY = 'QTD5RHQOA7JRTCMX'
+SCHEMA_REGISTRY_API_SECRET = 'Mrxw6EtMJoHvcrV3mGVDQ9+foLsmDOHbxUgJ0AfTMR0Ua/mWRWP4bbVtyvW78OKw'
+
+
+def sasl_conf():
+    sasl_conf = {'sasl.mechanism': SSL_MACHENISM,
+                 # Set to SASL_SSL to enable TLS support.
+                 #  'security.protocol': 'SASL_PLAINTEXT'}
+                 'bootstrap.servers': BOOTSTRAP_SERVER,
+                 'security.protocol': SECURITY_PROTOCOL,
+                 'sasl.username': API_KEY,
+                 'sasl.password': API_SECRET_KEY
+                 }
+    return sasl_conf
+
+
+def schema_config():
+    return {'url': ENDPOINT_SCHEMA_URL,
+
+            'basic.auth.user.info': f"{SCHEMA_REGISTRY_API_KEY}:{SCHEMA_REGISTRY_API_SECRET}"
+
+            }
+
+
+class Order:
+    def __init__(self, record: dict):
+        for k, v in record.items():
+            setattr(self, k, v)
+
+        self.record = record
+
+    @staticmethod
+    def dict_to_order(data: dict, ctx):
+        return Order(record=data)
+
+    def __str__(self):
+        return f"{self.record}"
+
+
+def main(topic):
+    schema_registry_conf = schema_config()
+    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+    schema_str = schema_registry_client.get_latest_version('restaurent-take-away-data-value').schema.schema_str
+    json_deserializer = JSONDeserializer(schema_str,
+                                         from_dict=Order.dict_to_order)
+
+    consumer_conf = sasl_conf()
+    consumer_conf.update({
+        'group.id': 'group1',
+        'auto.offset.reset': "earliest"})
+
+    consumer = Consumer(consumer_conf)
+    consumer.subscribe([topic])
+    messages = []
+    while True:
+        try:
+            # SIGINT can't be handled when polling, limit timeout to 1 second.
+            msg = consumer.poll(1.0)
+            if msg is None:
+                if len(messages) != 0:
+                    df = pd.DataFrame.from_records(messages)
+                    if not os.path.isfile('output.csv'):
+                        df.to_csv('output.csv', index=False)
+                    else:
+                        df.to_csv('output.csv', mode='a', header=False, index=False)
+                    messages = []
+                continue
+            order = json_deserializer(msg.value(), SerializationContext(msg.topic(), MessageField.VALUE))
+
+            if order is not None:
+                print("User record {}: order: {}\n"
+                      .format(msg.key(), order))
+                messages.append(order.record)
+
+        except KeyboardInterrupt:
+            break
+
+    consumer.close()
+
+
+main("restaurent-take-away-data")
